f6b5eb93db052db10e8cc41dc9f5216876146f40
Tess Craig <tessashleycraig89@gmail.com>
2017-12-22 09:09:21 -0600

    Commit 12 - adds event handler for deal button

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+//button
+$("#dealButton").click(function () {
+  ;
+});
 
+
+//flip card
+
+
+//deal
+
+
+//post character image
+
+
+//post quote
+
+
+//arrow
+
+
+//match alert
+
+
+//not a match alert
