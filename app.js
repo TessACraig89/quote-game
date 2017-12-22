666ec6ce56980f9ffd9f193e9cb2120e05359cf1
Tess Craig <tessashleycraig89@gmail.com>
2017-12-22 13:50:22 -0600

    Commit 13 - adds background image, js outline, and image folder with doctor, janeway, and seven images

--- app.js
+++ app.js
@@ -3,23 +3,58 @@ $("#dealButton").click(function () {
   ;
 });
 
+//character cards
+let characterCards = [{
+  name: "",
+  cardImage: ""
+}, {
+  name: "",
+  cardImage: ""
+}, {
+  name: "",
+  cardImage: ""
+}];
 
+//quote cards
+let quoteCards = [{
+  name: "",
+  cardImage: ""
+}, {
+  name: "",
+  cardImage: ""
+}, {
+  name: "",
+  cardImage: ""
+}];
+
+var cardsInPlay = [];
 //flip card
+const flipCard = function() {
+  var cardId = this.getAttribute('data-id');
+  cardsInPlay.push(characterCards[cardId].name);
+  this.setAttribute('src', characterCards[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
 
+//check for Match
+var checkForMatch = function() {
+  if () {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
 
-//deal
+//createBoard
+let createBoard = function() {
+  for (let i = 0; i < characterCards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', '');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
 
-
-//post character image
-
-
-//post quote
-
-
-//arrow
-
-
-//match alert
-
-
-//not a match alert
+createBoard();
