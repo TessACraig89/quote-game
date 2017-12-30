a01a9d84a35350e52d62f988ad286ed8a501b963
Tess Craig <tessashleycraig89@gmail.com>
2017-12-30 16:41:10 -0600

    Commit 18 - replaces createBoard function with for loop that creates and appends image element for every chracter, adds check quote match functions for each chracter

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// get quote button
-//
-
-//
-
-var quotes = [
-  {quote: 'Your appeal to my humanity is pointless.',
-    author:'Seven2'},
-  {quote: 'there is coffee in that nebula',
-    author: 'Janeway1'},
-  {quote: 'I will complain if I want to. It is comforting.',
-    author: 'Doctor3'},
+//quotes array
+let quotes = [
+  {
+  quote: 'Your appeal to my humanity is pointless.',
+  author:'Seven2'
+  },
+  {
+  quote: 'there is coffee in that nebula',
+  author: 'Janeway1'
+  },
+  {
+  quote: 'I will complain if I want to. It is comforting.',
+  author: 'Doctor3'
+  },
 ]
 
 //character cards
@@ -26,48 +28,49 @@ let characterCards = [
   name: "doctor",
   cardImage: "images/doctor_card.jpg"
   }
-];
+]
 
-//
+for (let i = 0; i < characterCards.length; i++) {
+  let card = document.createElement("img");
+  card.src = characterCards[i].cardImage;
+  card.id = characterCards[i].name;
+  let gameBoard = document.getElementById('game-board');
+  gameBoard.append(card)
+}
 
-// var cardsInPlay = [];
-//
-// var checkForMatch = function() {
-//   if (cardsInPlay[0] === cardsInPlay[1]) {
-//     alert("You found a match!");
-//   } else {
-//     alert("Sorry, try again.");
-//     location.reload();
-//   }
-// }
-
-var createBoard = function() {
-    var janewayCardElement = document.createElement('img');
-    janewayCardElement.setAttribute('src', 'images/janeway_card.jpg');
-
-    var doctorCardElement = document.createElement('img');
-    doctorCardElement.setAttribute('src', 'images/doctor_card.jpg');
-
-    var sevenCardElement = document.createElement('img');
-    sevenCardElement.setAttribute('src', 'images/seven_card.jpg');
-
-    var quoteCardElement = document.createElement('img');
-    sevenCardElement.setAttribute('src', 'images/seven_card.jpg');
-
-    document.getElementById('game-board').appendChild(janewayCardElement);
-
-    document.getElementById('game-board').appendChild(sevenCardElement);
-
-
-    document.getElementById('game-board').appendChild(doctorCardElement);
-
-};
-
-var elem2 = document.createElement("h5");
-  var source = quotes[Math.floor(Math.random()*quotes.length)];
+let elem2 = document.createElement("h5");
+  let source = quotes[Math.floor(Math.random()*quotes.length)];
   elem2.id = source.author;
   elem2.innerHTML = `"<i>${source.quote}</i>"`
-  var k = document.getElementById('quote');
+  let k = document.getElementById('quote');
   k.append(elem2)
 
-createBoard();
+let sevenElement = document.getElementById("seven");
+sevenElement.addEventListener("click",function(){
+let idk = document.getElementsByTagName('h5');
+  if (idk[0].id==='Seven2') {
+    alert('YES');
+  } else {
+    alert('NO');
+    }
+  })
+
+let janewayElement = document.getElementById("janeway");
+janewayElement.addEventListener("click",function(){
+let idk = document.getElementsByTagName('h5');
+    if (idk[0].id==='Janeway1') {
+      alert('YES');
+    } else {
+      alert('NO');
+      }
+    })
+
+let doctorElement = document.getElementById("doctor");
+doctorElement.addEventListener("click",function(){
+let idk = document.getElementsByTagName('h5');
+    if (idk[0].id==='Doctor3') {
+          alert('YES');
+    } else {
+          alert('NO');
+      }
+    })
