88c4813fdd8d488209ba7a611b54cc78fb00c9b9
Tess Craig <tessashleycraig89@gmail.com>
2017-12-30 15:32:53 -0600

    Commit 17 - adds quotes array, changes createBoard function to present character cards upon load, adds random quote generator

--- app.js
+++ app.js
@@ -1,61 +1,73 @@
-//button
-$("#dealButton").click(function () {
-  ;
-});
+// get quote button
+//
+
+//
+
+var quotes = [
+  {quote: 'Your appeal to my humanity is pointless.',
+    author:'Seven2'},
+  {quote: 'there is coffee in that nebula',
+    author: 'Janeway1'},
+  {quote: 'I will complain if I want to. It is comforting.',
+    author: 'Doctor3'},
+]
 
 //character cards
-let characterCards = [{
+let characterCards = [
+  {
   name: "janeway",
-  cardImage: "iimages/janeway_trans.jpg"
-}, {
+  cardImage: "images/janeway_card.jpg"
+  },
+  {
   name: "seven",
-  cardImage: "images/seven_trans.jpg"
-}, {
+  cardImage: "images/seven_card.jpg"
+  },
+  {
   name: "doctor",
-  cardImage: "images/doctor_trans.jpg"
-}];
-
-// quote cards
-// let quoteCards = [{
-//   name: "",
-//   cardImage: ""
-// }, {
-//   name: "",
-//   cardImage: ""
-// }, {
-//   name: "",
-//   cardImage: ""
-// }];
-
-var cardsInPlay = [];
-//flip card
-const flipCard = function() {
-  var cardId = this.getAttribute('data-id');
-  cardsInPlay.push(characterCards[cardId].name);
-  this.setAttribute('src', characterCards[cardId].cardImage);
-  if (cardsInPlay.length === 2) {
-    checkForMatch();
+  cardImage: "images/doctor_card.jpg"
   }
-}
+];
 
-var checkForMatch = function() {
-  // if () {
-  //   alert("You found a match!");
-  // } else {
-  //   alert("Sorry, try again.");
-    location.reload();
-  // }
-}
+//
 
-//createBoard
-let createBoard = function() {
-  for (let i = 0; i < characterCards.length; i++) {
-    let cardElement = document.createElement('img');
-    cardElement.setAttribute('src', 'images/back.jpg');
-    cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', flipCard);
-    document.getElementById('game-board').appendChild(cardElement);
-  }
+// var cardsInPlay = [];
+//
+// var checkForMatch = function() {
+//   if (cardsInPlay[0] === cardsInPlay[1]) {
+//     alert("You found a match!");
+//   } else {
+//     alert("Sorry, try again.");
+//     location.reload();
+//   }
+// }
+
+var createBoard = function() {
+    var janewayCardElement = document.createElement('img');
+    janewayCardElement.setAttribute('src', 'images/janeway_card.jpg');
+
+    var doctorCardElement = document.createElement('img');
+    doctorCardElement.setAttribute('src', 'images/doctor_card.jpg');
+
+    var sevenCardElement = document.createElement('img');
+    sevenCardElement.setAttribute('src', 'images/seven_card.jpg');
+
+    var quoteCardElement = document.createElement('img');
+    sevenCardElement.setAttribute('src', 'images/seven_card.jpg');
+
+    document.getElementById('game-board').appendChild(janewayCardElement);
+
+    document.getElementById('game-board').appendChild(sevenCardElement);
+
+
+    document.getElementById('game-board').appendChild(doctorCardElement);
+
 };
+
+var elem2 = document.createElement("h5");
+  var source = quotes[Math.floor(Math.random()*quotes.length)];
+  elem2.id = source.author;
+  elem2.innerHTML = `"<i>${source.quote}</i>"`
+  var k = document.getElementById('quote');
+  k.append(elem2)
 
 createBoard();
