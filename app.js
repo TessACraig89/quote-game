1b39bc5efe6506413dffb36e72bbeb2818bbe731
Tess Craig <tessashleycraig89@gmail.com>
2017-12-31 15:27:56 -0600

    Commit 22 - adds more quotes

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-let button = document.getElementById('button');
-
-
 //quotes array
 let quotes = [
-  {
-  quote: "Your appeal to my humanity is pointless.",
-  author:'Seven2'
-  },
   {
   quote: "There is coffee in that nebula.",
   author: 'Janeway1'
   },
   {
+  quote: "Oh, you know the story. Girl meets boy, girl reprograms boy's subroutines.",
+  author: 'Janeway1'
+  },
+  {
+  quote: "At ease before you sprain something.",
+  author: 'Janeway1'
+  },
+  {
+  quote: "I understand the concept of humor. It may not be apparent but I am often amused by human behavior.",
+  author: 'Seven2'
+  },
+  {
+  quote: "Your appeal to my humanity is pointless.",
+  author:'Seven2'
+  },
+  {
+  quote: "Fun will now commence.",
+  author:'Seven2'
+  },
+  {
   quote: "I'll complain if I want to. It's comforting.",
   author: 'Doctor3'
   },
+  {
+  quote: "You should know I'm a hologram and can't be bent, spindled, or mutilated, so don't bother trying.",
+  author: 'Doctor3'
+  },
+  {
+  quote: "The Borg: party-poopers of the galaxy.",
+  author: 'Doctor3'
+  }
 ]
 
 //character cards
@@ -34,6 +55,11 @@ let characterCards = [
 ]
 
 //for loop that loops through character cards array and creates and appends character card image to game board
+  // creates image element and stores in variable card
+  // sets img src to be the cardImage url value
+  //sets card image element's id to be the name value
+  // get game board element by id and stores
+  // card is appended to gameBoard
 for (let i = 0; i < characterCards.length; i++) {
   let card = document.createElement("img");
   card.src = characterCards[i].cardImage;
@@ -42,35 +68,23 @@ for (let i = 0; i < characterCards.length; i++) {
   gameBoard.append(card)
 }
 
+// creates h3 element
+  // get the largest index less then or equal to quotes.length a random
 
-let elem2 = document.createElement("h5");
+let elem2 = document.createElement("h3");
   let source = quotes[Math.floor(Math.random()*quotes.length)];
   elem2.id = source.author;
   elem2.innerHTML = source.quote
   let quote = document.getElementById('quote');
   quote.append(elem2)
 
-// button.addEventListener("click",function(){
-//   let quote = document.getElementById('quote');
-//   quote.append(elem2)
-// })
 
-let counter = 10
 
-let sevenElement = document.getElementById("seven");
-sevenElement.addEventListener("click",function(){
-let quoteElement = document.getElementsByTagName('h5');
-  if (quoteElement[0].id==='Seven2') {
-    alert('Correct');
-    window.location.reload(true);
-  } else {
-    alert('Incorrect, try again!');
-    }
-  })
+
 
 let janewayElement = document.getElementById("janeway");
 janewayElement.addEventListener("click",function(){
-let quoteElement = document.getElementsByTagName('h5');
+let quoteElement = document.getElementsByTagName('h3');
     if (quoteElement[0].id==='Janeway1') {
       alert('Correct');
       window.location.reload(true);
@@ -79,9 +93,22 @@ let quoteElement = document.getElementsByTagName('h5');
       }
     })
 
+let sevenElement = document.getElementById("seven");
+sevenElement.addEventListener("click",function(){
+let quoteElement = document.getElementsByTagName('h3');
+  if (quoteElement[0].id==='Seven2') {
+    alert('Correct');
+    window.location.reload(true);
+  } else {
+    alert('Incorrect, try again!');
+    }
+  })
+
+
+
 let doctorElement = document.getElementById("doctor");
 doctorElement.addEventListener("click",function(){
-let quoteElement = document.getElementsByTagName('h5');
+let quoteElement = document.getElementsByTagName('h3');
     if (quoteElement[0].id==='Doctor3') {
           alert('Correct');
           window.location.reload(true);
