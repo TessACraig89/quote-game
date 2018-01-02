2f4e3dde42382d75861db426f52fb9a5b5272bea
Tess Craig <tessashleycraig89@gmail.com>
2018-01-02 08:59:58 -0600

    Commit 31 - styles quote

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ janewayElement.addEventListener("click",function(){
 let quoteElement = document.getElementsByTagName('h3');
     if (quoteElement[0].id==='Janeway1') {
       if (attemptsLeft===3){
-        correctPopup(); window.location.reload(true);
+        alert('You picked Captain Janeway! Congratulations, you matched correctly on your first try!'); window.location.reload(true);
       }
       if (attemptsLeft<3 && attemptsLeft>=2){
         alert('You picked Captain Janeway! You answered correctly on your second try!');
@@ -128,11 +128,7 @@ let quoteElement = document.getElementsByTagName('h3');
           }
     })
 
-    // When the user clicks on <div>, open the popup
-let correctPopup = () => {
-    let popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
+
 
 let sevenElement = document.getElementById("seven");
 sevenElement.addEventListener("click",function(){
@@ -205,4 +201,63 @@ let quoteElement = document.getElementsByTagName('h3');
     //     quote.parentNode.removeChild(element);
     //   }
 
+//popup function
+// let correctPopup = () => {
+//     let popup = document.getElementById("myPopup");
+//     popup.classList.toggle("show");
+// }
+  //html
+  // <div class="popup" onclick="correctPopup()">Click me!
+  //   <span class="popuptext" id="myPopup">You picked Captain Janeway! Congratulations, you matched correctly on your first try!</span>
+  // </div>
+  //css
+  /* Popup container */
+  // .popup {
+  //     position: relative;
+  //     display: inline-block;
+  //     cursor: pointer;
+  // }
+  // .popup .popuptext {
+  //     visibility: hidden;
+  //     width: 160px;
+  //     background-color: #555;
+  //     color: #fff;
+  //     text-align: center;
+  //     border-radius: 6px;
+  //     padding: 8px 0;
+  //     position: absolute;
+  //     z-index: 1;
+  //     bottom: 125%;
+  //     left: 50%;
+  //     margin-left: -80px;
+  // }
   //
+  // /* Popup arrow */
+  // .popup .popuptext::after {
+  //     content: "";
+  //     position: absolute;
+  //     top: 100%;
+  //     left: 50%;
+  //     margin-left: -5px;
+  //     border-width: 5px;
+  //     border-style: solid;
+  //     border-color: #555 transparent transparent transparent;
+  // }
+  //
+  // /* Toggle this class when clicking on the popup container (hide and show the popup) */
+  // .popup .show {
+  //     visibility: visible;
+  //     -webkit-animation: fadeIn 1s;
+  //     animation: fadeIn 1s
+  // }
+  //
+  // /* Add animation (fade in the popup) */
+  // @-webkit-keyframes fadeIn {
+  //     from {opacity: 0;}
+  //     to {opacity: 1;}
+  // }
+  //
+  // @keyframes fadeIn {
+  //     from {opacity: 0;}
+  //     to {opacity:1 ;}
+  // }
